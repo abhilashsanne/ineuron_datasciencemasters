a5f0397b4c21429026b1e7ede04ae65e696d1c28
Abhilash Sanne <sanne.abhi@gmail.com>
2022-01-08 11:43:25 +0530

    Create 5.list_concat.py

---
+++ assignments/python/functions/5.list_concat.py
@@ -0,0 +1,14 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def list_concat(lists):
+    """
+    Joins given list of lists into a single list
+    """
+    final_list = []
+    for sublist in lists:
+        final_list.extend(sublist)
+    return final_list
+
+a = [[1,2,3], [4,5,6], [7,8,9], [10]]
+print(list_concat(a))
